bb04becae4c46f91cdbaeec0e87561ba64266895
B3an5 <beantheo@gmail.com>
2019-05-02 19:41:22 -0600

    film crawl start

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Crawl from 'react-star-wars-crawl'
+import 'react-star-wars-crawl/lib/index.css'
 
 export default function Splash(props) {
   return (
     <div>
-      <h1>{props.film.title}</h1>
+      {/* <h1>{props.film.title}</h1>
       <h1><span>Released:</span>{props.film.release_date}</h1>
-      <p>{props.film.opening_crawl}</p>
+      <p>{props.film.opening_crawl}</p> */}
+      <Crawl 
+        title={props.film.title}
+        subTitle={props.film.release_date}
+        text={props.film.opening_crawl}
+      />
       <button onClick={props.splashButton}>Continue</button>
     </div>
   )
