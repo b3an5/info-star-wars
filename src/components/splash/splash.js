45bf1018993b7ab32f9dec0c8ebaefb5b92fecf9
B3an5 <beantheo@gmail.com>
2019-05-05 16:43:30 -0600

    splash tests

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -5,9 +5,6 @@ import 'react-star-wars-crawl/lib/index.css'
 export default function Splash(props) {
   return (
     <div>
-      {/* <h1>{props.film.title}</h1>
-      <h1><span>Released:</span>{props.film.release_date}</h1>
-      <p>{props.film.opening_crawl}</p> */}
       <Crawl 
         title={props.film.title}
         subTitle={props.film.release_date}
